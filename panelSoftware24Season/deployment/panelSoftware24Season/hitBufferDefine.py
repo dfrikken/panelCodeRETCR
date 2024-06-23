3b39e6c5a9c7c7a50dc57c054b20c61d2dd1f97c
dfrikken <51166509+dfrikken@users.noreply.github.com>
2024-06-23 11:46:06 -0400

    Update hitBufferDefine.py

--- panelSoftware24Season/deployment/panelSoftware24Season/hitBufferDefine.py
+++ panelSoftware24Season/deployment/panelSoftware24Season/hitBufferDefine.py
@@ -1050,7 +1050,7 @@ def fitThreshCurve(panel, dataDir,targetRate):
         if f'panel{panel}' in i:
             print(f'threshold sweep for panel {panel} found')
             myFile = f'{threshDir}/{i}'
-            print(myFile)
+            print(f'myFile is {threshDir}/{i}')
             break
             #print(myFile)
     print(myFile)

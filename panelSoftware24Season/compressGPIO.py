c4a39dc14ad63960f598b149d6edd2b02cb69a6b
dfrikken <51166509+dfrikken@users.noreply.github.com>
2024-07-23 10:46:59 -0400

    Update compressGPIO.py

--- panelSoftware24Season/compressGPIO.py
+++ panelSoftware24Season/compressGPIO.py
@@ -7,10 +7,11 @@ dataDir = '/home/retcr/deployment/panelSoftware24Season/runs'
 
 
 mainDir = os.listdir(dataDir)
+gpioCounter=0
 
 for n,i in enumerate(mainDir):
     #print(i.strip('\n')[:4])
-    if n > 10:
+    if n > 2:
         break
     if (i.strip(('\n'))[:4] == '2024'):
         print(i)
@@ -21,12 +22,12 @@ for n,i in enumerate(mainDir):
 
         for m, j in enumerate(dayDirList):
             print(j)
-            if m > 10:
+            if m > 2:
                 break
             runDir = f'{newDir}/{j}'
             print(runDir)
-            #runDirList = os.listdir(runDir)
-            #print(runDirList)
+            runDirList = os.listdir(runDir)
+            print(runDirList)
         print('\n')
 
             

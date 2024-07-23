5295d30050c8927b6dad84244b86d38923c1642b
dfrikken <51166509+dfrikken@users.noreply.github.com>
2024-07-23 10:50:30 -0400

    Update compressGPIO.py

--- panelSoftware24Season/compressGPIO.py
+++ panelSoftware24Season/compressGPIO.py
@@ -11,10 +11,10 @@ gpioCounter=0
 
 for n,i in enumerate(mainDir):
     #print(i.strip('\n')[:4])
-    if n > 2:
-        break
+    #if n > 2:
+    #    break
     if (i.strip(('\n'))[:4] == '2024'):
-        print(i)
+        #print(i)
         newDir = f'{dataDir}/{i}/hitBufferRuns'
         #print(f'{newDir}')
 
@@ -22,14 +22,17 @@ for n,i in enumerate(mainDir):
 
         for m, j in enumerate(dayDirList):
             print(j)
-            if m > 2:
-                break
+            #if m > 2:
+            #    break
             runDir = f'{newDir}/{j}'
-            print(runDir)
+            #print(runDir)
             runDirList = os.listdir(runDir)
             #print(runDirList)
             if 'gpioMon.txt' in runDirList:
-                print('gpio file found')
+                #print('gpio file found')
+                gpioCounter+=1
         print('\n')
+
+print(f'gpio monitor files found {gpioCounter}')
 
             
